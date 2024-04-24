418f79a61dbef97d5eae87baeb767c21437989b3
asteng88 <142433649+asteng88@users.noreply.github.com>
2024-04-24 19:18:18 -0400

    Add files via upload

--- tolerance.py
+++ tolerance.py
@@ -1,25 +1,34 @@
-# Tolerance Helper 
-# @author: Andrew Thomas
-# @date: 2023-12-13
-# @description: A simple tool to help calculate tolerances and check if a measurement is within tolerance
-# @usage: Enter the upper and lower tolerance values and the nominal value, then enter the measurement value and click calculate
-# @Note: Input masking is controlled through the tolerance.ui file in Qt designer
-# @license: GNU GPL v3
-# @version: 1.2
+import base64
 
+def encode(data):
+    try:
+        # Standard Base64 Encoding
+        encodedBytes = base64.b64encode(data.encode("utf-8"))
+        return str(encodedBytes, "utf-8")
+    except:
+        return ""
+    
+def decode(data):
+    try:
+        message_bytes = base64.b64decode(data)
+        return message_bytes.decode('utf-8')
+    except:
+        return ""
+
+code = encode(r"""
+                         
 import os, sys
 from PyQt6 import uic, QtWidgets
 from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QMessageBox)
 
-basedir = os.path.dirname(__file__)  # set a path as the same directory as this file
-os.chdir(basedir)  # change the working directory to the same directory as this file
+basedir = os.path.dirname(__file__)  # set a path as the same directory
+os.chdir(basedir)  # change the working directory to the same directory
 
 # this block ensures that the ICON is displayed on the taskbar in Windows
 try:
     from ctypes import windll
-
-    my_appid = 'com.laserslayer.tolerancehelper.tolerancehelper.1.1'
+    my_appid = 'com.asteng88.tolerancehelper.tolerancehelper.1.0'
     windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_appid)
 except ImportError:
     pass
@@ -31,14 +40,16 @@ class UiMainWindow(QMainWindow):
         uic.loadUi(ui, self) # load the UI file
 
         # Connect signals to slots
+        self.aboutButton = self.findChild(QtWidgets.QToolButton, 'aboutButton')
         self.upperTolLCD = self.findChild(QtWidgets.QLCDNumber, 'lower_tolerance_out')
         self.lowerTolLCD = self.findChild(QtWidgets.QLCDNumber, 'upper_tolerance_out')
         self.nominalLCD = self.findChild(QtWidgets.QLCDNumber, 'nominal_out')
         self.lowerTolInput = self.findChild(QtWidgets.QLineEdit, 'lower_tolerance_in')
-        self.lowerTolInput.textChanged.connect(self.addLotTolNomTol)
+        self.lowerTolInput.textChanged.connect(self.addLowTolNomTol)
         self.upperTolInput = self.findChild(QtWidgets.QLineEdit, 'upper_tolerance_in')
         self.upperTolInput.textChanged.connect(self.addUppTolNomTol)
         self.nominalInput = self.findChild(QtWidgets.QLineEdit, 'nominal_in')
+        self.nominalInput.textChanged.connect(self.enterNominal)
         self.measurementInput = self.findChild(QtWidgets.QLineEdit, 'measurement_in')
         self.outputLabel = self.findChild(QtWidgets.QLabel, 'output_label')
         self.quitButton = self.findChild(QtWidgets.QPushButton, 'quitButton')
@@ -47,85 +58,123 @@ class UiMainWindow(QMainWindow):
         self.clearText.clicked.connect(self.clearTextAction)
         self.calculateButton = self.findChild(QtWidgets.QPushButton, 'calculateButton')
         self.calculateButton.clicked.connect(self.calculateTolerance)
+        self.aboutButton.clicked.connect(self.aboutAction)
         self.clearMeasurementButoon = self.findChild(QtWidgets.QPushButton, 'clearMeasurementButton')
         self.clearMeasurementButoon.clicked.connect(self.clearMeasurement)
-        self.nominalInput.setFocus() #set the focus to the nominal input box
-        self.nominalInput.setSelection(0,0) #set the cursor to the start of the nominal input box
         
+    
+    def enterNominal(self, text):
+        try:
+            if text.startswith("."):
+                text = "0" + text
+                self.nominalInput.setText(text)
+            else:
+                text = str(round(float(text), 3))  # Round the text to 3 decimal places
+                self.nominalLCD.display(text)
+                return
+        except ValueError:
+            return
+    
     def addUppTolNomTol(self, text):
-        if text.startswith("."):
-            text = "0" + text
-            self.upperTolInput.setText(text)  
-        else:
-            addUpper = float(self.nominalInput.text()) + float(text)
-            self.upperTolLCD.display(addUpper)
+        try:
+            if text.startswith("."):
+                text = "0" + text
+                round(float(text),3)
+                self.upperTolInput.setText(text)  
+            else:
+                addUpper = round(float(self.nominalInput.text()) + float(text),3)
+                self.upperTolLCD.display(addUpper)
+                return
+        except ValueError:
             return
             
-    def addLotTolNomTol(self, text):
-        if text.startswith("."):
-            text = "0" + text
-            self.lowerTolInput.setText(text)
-        else:
-            addLower = float(self.nominalInput.text()) - float(text)
-            self.lowerTolLCD.display(addLower)
+    def addLowTolNomTol(self, text):
+        try:
+            if text.startswith("."):
+                text = "0" + text
+                round(float(text),3)
+                self.lowerTolInput.setText(text)
+            else:
+                addLower = round(float(self.nominalInput.text()) - float(text),3)
+                self.lowerTolLCD.display(addLower)
+                return
+        except ValueError:
             return
-
+    
     def quitAction(self):
          QApplication.quit()
          return
     
-    def resetLCDstyle(self):
+    def resetStyle(self):
         for lcd in (self.upperTolLCD, self.lowerTolLCD, self.nominalLCD):
-            lcd.setStyleSheet("color: black")
+            lcd.setStyleSheet("color: black; background-color: lightgrey")
+        for inputs in (self.upperTolInput, self.lowerTolInput, self.nominalInput, self.measurementInput, self.outputLabel):
+            inputs.setStyleSheet("color: black; background-color: white")
         return
     
     def clearTextAction(self):
         for inputs in (self.upperTolInput, self.lowerTolInput, self.nominalInput, self.measurementInput):
-            inputs.setText("0.000")
+            inputs.setText("")
         self.outputLabel.clear()
-        self.resetLCDstyle()
+        self.resetStyle()
         for lcd in (self.upperTolLCD, self.lowerTolLCD, self.nominalLCD):
             lcd.display(0)
         self.nominalInput.setFocus()
-        self.nominalInput.setSelection(0,0)
         return     
     
     def clearMeasurement(self):
-        self.measurementInput.setText("0.000")
+        self.measurementInput.clear()
         self.outputLabel.clear()
-        self.resetLCDstyle()
+        self.resetStyle()
         self.measurementInput.setFocus()
-        self.measurementInput.setSelection(0,0)
+        return
+
+    def aboutAction(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Icon.Information)
+        msg.setText("Tolerance Helper V1.0")
+        msg.setInformativeText('This is a simple tolerance calculator that \nchecks if a measurement is within tolerance.\n\nWritten by Andrew Thomas. \n\nPlease report or fix any bugs at: \nhttps://github.com/asteng88/tolerance_helper.')
+        msg.setWindowTitle("About Tolerance Helper")
+        msg.setWindowIcon(QIcon(icon_1))
+        msg.exec()
         return
 
     def calculateTolerance(self):
-        if (not self.upperTolInput.text() or not self.lowerTolInput.text() or not self.nominalInput.text() or not self.measurementInput.text()):
-            self.outputLabel.setStyleSheet("color: black")
-            self.outputLabel.setText('Please enter all values')
-            return
-        else:
-            upperTolTotal = float(self.nominalInput.text()) + float(self.upperTolInput.text())
-            lowerTolTotal = float(self.nominalInput.text()) - float(self.lowerTolInput.text())
+        try:
+            upperTolTotal = round(float(self.nominalInput.text()) + float(self.upperTolInput.text()), 3)
+            lowerTolTotal = round(float(self.nominalInput.text()) - float(self.lowerTolInput.text()), 3)
             self.upperTolLCD.display(upperTolTotal)
             self.lowerTolLCD.display(lowerTolTotal)
             self.nominalLCD.display(self.nominalInput.text())
-            
+        
             # Check if the measurement is within the tolerance
-            if float(self.measurementInput.text()) <= upperTolTotal and float(self.measurementInput.text()) >= lowerTolTotal:
-                for setGreen in (self.outputLabel, self.upperTolLCD, self.lowerTolLCD):
-                    setGreen.setStyleSheet("color: green")
+            measurement = round(float(self.measurementInput.text()), 3)
+            if measurement <= upperTolTotal and measurement >= lowerTolTotal:
+                for setGreen in (self.outputLabel, self.upperTolLCD, self.lowerTolLCD, self.measurementInput):
+                    setGreen.setStyleSheet("color: black; background-color: lightgreen")
                 self.outputLabel.setText('Measurement is within tolerance')
             else:
-                self.outputLabel.setStyleSheet("color: red")
+                self.outputLabel.setStyleSheet("color: black; background-color: pink")
                 self.outputLabel.setText('Measurement is NOT within tolerance')
-                if float(self.measurementInput.text()) > upperTolTotal:
-                    self.upperTolLCD.setStyleSheet("color: red")
+                self.measurementInput.setStyleSheet("color: black; background-color: pink")
+                if measurement > upperTolTotal:
+                    self.upperTolLCD.setStyleSheet("color: black; background-color: pink")
                 else:
-                    self.upperTolLCD.setStyleSheet("color: green")
-                if float(self.measurementInput.text()) < lowerTolTotal:
-                    self.lowerTolLCD.setStyleSheet("color: red")
+                    self.upperTolLCD.setStyleSheet("color: black; background-color: lightgreen")
+                if measurement < lowerTolTotal:
+                    self.lowerTolLCD.setStyleSheet("color: black; background-color: pink")
                 else:
-                    self.lowerTolLCD.setStyleSheet("color: green")
+                    self.lowerTolLCD.setStyleSheet("color: black; background-color: lightgreen")
+            #return
+    
+        except ValueError:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Icon.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('Please ensure ALL fields are complete and are numeric')
+            msg.setWindowTitle("Error: Input Error")
+            msg.setWindowIcon(QIcon(icon_1))
+            msg.exec()
         return
 
 app = QApplication(sys.argv)
@@ -134,3 +183,6 @@ icon_1 = os.path.join(basedir, 'toleranceicon.png')
 window.setWindowIcon(QIcon(icon_1))
 window.show()
 app.exec()
+
+""")
+exec(decode(code))
